59c63c84b86556fa3e9e3e5d2a33441a679d36b4
lilo-error404 <murilosouzagames2005@gmail.com>
2025-08-26 09:04:54 -0300

    tem duas opções (recomendo a nao comentada)

--- App.js
+++ App.js
@@ -66,3 +66,96 @@ export default function App() {
     </View>
   );
 }
+
+// import './App.css'
+// import HelloWorld from './hello-world/HelloWorld'
+// import Calculadora from './calculadora/Calculadora'
+// import CalculadoraIMC from './calculadora-imc/CalculadoraIMC'
+
+// function App() {
+//   return (
+//     // <HelloWorld />
+//     <CalculadoraIMC />
+//   );
+// }
+
+// export default App;
+// import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+// import { useState } from 'react';
+// import { styles } from './style';
+
+// export default function App() {
+
+//   const [peso, setPeso] = useState('');
+//   const [altura, setAltura] = useState('');
+//   const [imc, setImc] = useState('');
+//   const [estado, setEstado] = useState('');
+//   const [textoImc, setTextoImc] = useState('');
+
+//   const calculaImc = () => {
+//     if (peso != '' && altura != '' && !isNaN(peso) && !isNaN(altura)) {
+//       const valorImc = peso / altura ** 2;
+//       setImc(valorImc.toFixed(1));
+//       setTextoImc('Imc: ');
+
+//       const estadoPessoa = (valorImc < 17) ? 'Muito abaixo do peso' : 
+//       (valorImc >= 17 && valorImc < 18.5) ? 'Abaixo do peso' :
+//       (valorImc >= 18.5 && valorImc < 25) ? 'Peso normal' : 
+//       (valorImc >= 25 && valorImc < 30) ? 'Acima do peso' :
+//       (valorImc >= 30 && valorImc < 35) ? 'Obesidade 1' :
+//       (valorImc >= 35 && valorImc < 40) ? 'Obesidade 2' : 'Obesidade 3';
+
+//       setEstado(estadoPessoa);
+//     }
+//   }
+
+
+//   const limpar = () => {
+//     setPeso('');
+//     setAltura('');
+//     setImc('');
+//     setEstado('');
+//     setTextoImc('');
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.titulo}>Cálculo do IMC</Text>
+
+//       <TextInput
+//         style={styles.input}        
+//         placeholder="Seu peso..."
+//         placeholderTextColor="#002"
+//         keyboardType='numeric'
+//         value={peso}
+//         onChangeText={ (peso) => setPeso(peso)}
+//       />
+
+//       <TextInput
+//         style={styles.input}        
+//         placeholder="Sua altura..."
+//         placeholderTextColor="#002"
+//         keyboardType='numeric'
+//         value={altura}
+//         onChangeText={ (altura) => setAltura(altura)}
+//       />
+
+//       <View style={{ flexDirection: "row" }}>
+//         <View>
+//           <TouchableOpacity style={styles.buttonEnviar} onPress={calculaImc}>
+//             <Text style={styles.buttonText}>Enviar</Text>
+//           </TouchableOpacity>
+//         </View>
+//         <View>
+//           <TouchableOpacity style={styles.buttonLimpar} onPress={limpar}>
+//             <Text style={styles.buttonText}>Limpar</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+
+//       <Text style={styles.output}>{textoImc} {imc}</Text>
+//       <Text style={styles.output}>{estado}</Text>
+
+//     </View>
+//   )
+// }
